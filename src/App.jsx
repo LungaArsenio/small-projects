fa8481209685b2c48f77b758741209cd6eb02abe
LungaArsenio <lungaarsenio0@gmail.com>
2024-10-18 05:29:23 -0500

    adding the Tailwind configuration and base classes for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+
+      let interval;
+
+      if (running) {
+        interval = setInterval(() => {
+          setTime((prevTime) => prevTime + 10);
+        }, 10);
+      } else if (!running) {
+        clearInterval(interval);
+      }
+    return () => clearInterval(interval);
+    }, [running]
+  );
 
   return (
-    <>
-      <h1>Stopwatch</h1>
-    </>
+    <div className='flex flex-col items-center justify-start py-8 h-screen'>
+      <h1 className='text-2xl font-semibold '>Stopwatch</h1>
+
+      <div className='text-xl font-semibold py-4'>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:  </span>
+        <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
+        {running ? <button className="border rounded-lg py-1 px-3" onClick={() => { setRunning(false) }}>Stop</button>
+          :
+          <button className="border rounded-lg py-1 px-2.5" onClick={() => { setRunning(true) }}>Start</button>
+        }
+
+        <button className='border rounded-lg py-1 px-2.5' onClick={() => {setTime(0)}}>Reset</button>
+      </div>
+    </div>
   )
 }
 
