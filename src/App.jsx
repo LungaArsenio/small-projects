3d017e8ad52a481744c1843bb9de7089b9e6a921
LungaArsenio <lungaarsenio0@gmail.com>
2024-10-23 10:06:30 -0500

    fixed styling, defined the fon-size rules on tailwind setting

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,16 @@ function App() {
   );
 
   return (
-    <div className='flex flex-col items-center justify-start py-8 h-screen'>
-      <h1 className='text-2xl font-semibold '>Stopwatch</h1>
+    <div className=' flex flex-col align-justify-center py-8 h-screen'>
+      <h1 className='text-5xl font-semibold '>Stopwatch</h1>
 
-      <div className='text-xl font-semibold py-4'>
+      <div className='text-2xl mx-auto font-semibold py-4'>
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:  </span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)}</span>
       </div>
-      <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
+
+      <div className=' max-w-sm flex flex-row justify-evenly'>
         {running ? <button className="border rounded-lg py-1 px-3" onClick={() => { setRunning(false) }}>Stop</button>
           :
           <button className="border rounded-lg py-1 px-2.5" onClick={() => { setRunning(true) }}>Start</button>
